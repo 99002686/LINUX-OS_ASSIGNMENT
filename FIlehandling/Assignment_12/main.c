beca1befb8ff686ae5f6e141e70e5af849addeda
99002686 <71485192+99002686@users.noreply.github.com>
2020-10-19 22:12:19 +0530

    Update main.c

--- FIlehandling/Assignment_12/main.c
+++ FIlehandling/Assignment_12/main.c
@@ -11,47 +11,45 @@
 struct rlimit maxResourceAllocation(void)
 {
     struct rlimit limitproc;
-    if(getrlimit(RLIMIT_NPROC, &limitproc) < 0)
+    if(getrlimit(RLIMIT_NOFILE, &limitproc) < 0)
     {
-        fprintf(stderr, "%s\n", strerror(errno));
+        fprintf(stderr, "maxResource: %s\n", strerror(errno));
         exit(0);
     }
     return limitproc;
-}
-
-void checkSetr(struct rlimit *resource)
-{
-    checkSetr(resource);
-    if(setrlimit(RLIMIT_NOFILE, resource) < 0) 
-        fprintf(stderr, "%s\n", strerror(errno)); 
 }
 
 struct rlimit getNewResource(void)
 {
-    struct rlimit limitproc;
+    struct rlimit limitproc, newProc;
     printf("\nEnter New Soft Limit:");
     scanf("%ld", &limitproc.rlim_cur);
     printf("\nEnter New Hard Limit:");
     scanf("%ld", &limitproc.rlim_max);
-    
-    if(getrlimit(RLIMIT_NPROC, &limitproc) < 0)
+    if(setrlimit(RLIMIT_NOFILE, &limitproc) < 0) 
     {
-        fprintf(stderr, "%s\n", strerror(errno));
-        exit(0);
+        fprintf(stderr, "setlimit: %s\n", strerror(errno)); 
+        exit(1);
     }
-    return limitproc;
+
+    if(getrlimit(RLIMIT_NOFILE, &newProc) < 0)
+    {
+        fprintf(stderr, "getrLimit:%s\n", strerror(errno));
+        exit(2);
+    }
+    printf("New limits soft limit= %ld \t hard limit= %ld \n", 
+        newProc.rlim_cur, newProc.rlim_max);
+    return newProc;
 }
 
 int main()
 {
-    struct rlimit *oldLimit, *newLimit, setLimit;
+    struct rlimit *oldLimit;
     *oldLimit = maxResourceAllocation();
     printf(" OldLimit soft limit= %ld \t OldLimit hard limit= %ld \n", 
         oldLimit->rlim_cur, oldLimit->rlim_max);  
 
-    *newLimit = getNewResource();
-    printf("New limits -> soft limit= %ld \t hard limit= %ld \n", 
-        newLimit->rlim_cur, newLimit->rlim_max); 
-
+    getNewResource(); 
+    
     return 0;
 }
